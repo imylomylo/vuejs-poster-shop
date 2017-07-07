df1ff0e96c813e7884d4db21bf2349fbb625f0ca
mylo <imylomylo@gmail.com>
2017-07-07 12:01:49 +1000

    project 1 85% complete from course

--- public/script.js
+++ public/script.js
@@ -1,1 +1,72 @@
-console.log('It works.');
+console.log(Vue);
+var PRICE = 9.99;
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [],
+        cart: [],
+        newSearch: 'anime',
+        lastSearch: '',
+        loading: false,
+        price: PRICE
+    },
+    methods: {
+        onSubmit: function(){
+            this.items = [] ;
+            this.loading = true ;
+            this.$http
+            .get('/search/'.concat(this.newSearch))
+            .then(function(res){
+                this.lastSearch = this.newSearch;
+                this.items = (res.data);
+                this.loading = false ;
+            });          
+        },
+        addItem: function(index){
+            var item = this.items[index] ;
+            item.price = PRICE ;
+            this.total += item.price ;
+            var found = false ;
+            for (var i = 0 ; i < this.cart.length; i++){
+                if (this.cart[i].id === item.id){
+                    found = true ;
+                    this.cart[i].qty++;
+                    break;
+                }
+            }
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: item.price
+                });
+            }
+        },
+        inc: function(item){
+            item.qty++;
+            this.total+=item.price;
+        },
+        dec: function(item){
+            item.qty--;
+            this.total-=item.price;
+            if (item.qty <= 0){
+                for (var i = 0 ; i < this.cart.length; i++ ){
+                    if (this.cart[i].id === item.id){
+                        this.cart.splice(i,1);
+                        break;
+                    }
+                }
+            }
+        }
+    },
+    filters: {
+        currency: function(price){
+            return '$'.concat(price.toFixed(2)) ;
+        }
+    },
+    mounted: function(){
+        this.onSubmit();
+    }
+});
